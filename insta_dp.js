cd9947cec7cfd7e89ac961afedcc8d4fde9bb9d1
Abhishek Goyal <abhishek252167@gmail.com>
2019-10-22 06:37:19 +0530

    Call Instagram API to get profile's JSON

--- insta_dp.js
+++ insta_dp.js
@@ -2,6 +2,8 @@ async function downloadBlobs(userName) {
     userName = userName.split(" ");
     for(let i = 0; i < userName.length; i++) {
         console.log(userName[i])
-        
+        let instaURL = 'https://www.instagram.com/'+userName[i]+'/?__a=1'
+        console.log(instaURL)
+        let instaFetch = await fetch(instaURL)
     }
 }
