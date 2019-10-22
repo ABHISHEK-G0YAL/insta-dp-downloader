75bbd89e966a5d16bf5933f89181b31f7c0167ca
Abhishek Goyal <abhishek252167@gmail.com>
2019-10-22 09:23:41 +0530

    Add function to download BLOB

--- insta_dp.js
+++ insta_dp.js
@@ -1,4 +1,11 @@
 async function downloadBlobs(userName) {
+    function downloadBlob(blob, filename) {
+        const url = URL.createObjectURL(blob);
+        const a = document.createElement('a');
+        a.href = url;
+        a.download = filename || 'download';
+        a.click();
+    }
     userName = userName.split(" ");
     for(let i = 0; i < userName.length; i++) {
         console.log(userName[i])
@@ -10,6 +17,10 @@ async function downloadBlobs(userName) {
             let profilePicURL = instaJSON.graphql.user.profile_pic_url_hd
             console.log(profilePicURL)
             let profilePicFetch = await fetch(profilePicURL)
+            if(profilePicFetch.ok) {
+                let profilePicBlob = await profilePicFetch.blob()
+                downloadBlob(profilePicBlob, userName[i] + '.jpg')
+            }
         }
         else {
             console.log("HTTP-Error: " + instaFetch.status)
