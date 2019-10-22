6ec2dd8970db7bde543df94cf4918b7e6d815063
Abhishek Goyal <abhishek252167@gmail.com>
2019-10-22 07:03:25 +0530

    Get profile pic URL from JSON and fetch it

--- insta_dp.js
+++ insta_dp.js
@@ -5,5 +5,14 @@ async function downloadBlobs(userName) {
         let instaURL = 'https://www.instagram.com/'+userName[i]+'/?__a=1'
         console.log(instaURL)
         let instaFetch = await fetch(instaURL)
+        if(instaFetch.ok) {
+            let instaJSON = await instaFetch.json()
+            let profilePicURL = instaJSON.graphql.user.profile_pic_url_hd
+            console.log(profilePicURL)
+            let profilePicFetch = await fetch(profilePicURL)
+        }
+        else {
+            console.log("HTTP-Error: " + instaFetch.status)
+        }
     }
 }
